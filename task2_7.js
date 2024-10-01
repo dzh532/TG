2e85e4e346a4455a536fd6da3d85a47d275f5dfa
Dmitriy <dmitrizhivoderov@mail.ru>
2024-10-01 13:09:43 +0400

    задание: вывод изолированных вершин

---
+++ task2_7.js
@@ -0,0 +1,20 @@
+const Graph = require('./graph1');
+
+let myGraph = new Graph(true, false);
+
+console.log('Создали тестовый граф: ');
+myGraph = myGraph.createCompleteGraph(3);
+console.log(myGraph.toString());
+
+console.log('Добвили вершину Е ');
+myGraph.addVertex('E');
+
+const isolatedVertices = myGraph.findIsolatedVertices();
+
+if (isolatedVertices.length > 0) 
+    console.log('Изолированные вершины:', isolatedVertices);
+else
+    console.log('Изолированных вершин нет');
+
+myGraph.closeInput();
+
